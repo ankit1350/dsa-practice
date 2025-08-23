55ee57214443498a110d1cf8904032ffd199c381
Ankit Kumar <mrankitak135@gmail.com>
2025-08-23 18:45:52 +0000

    DSA Practice - Added solutions on 2025-08-23 18:45

--- arrays/README.md
+++ arrays/README.md
@@ -4,3 +4,4 @@
 |------|----------|----------|
 | two_sum.py | Two Sum Problem | <approach> |
 | max_subarray.py | Maximum Subarray | <approach> |
+| sum_of_elements.py | Sum of Array Elements | <approach> |
---
+++ arrays/sum_of_elements.java
@@ -0,0 +1,21 @@
+"""
+Problem: Sum of Array Elements
+Link: https://www.geeksforgeeks.org/batch/i2it-coding-practice/track/arrays-problems-i2it/problem/sum-all-array-elements
+
+Approach:
+- <Describe your approach here>
+- Time: O(?), Space: O(?)
+
+Notes:
+- <Edge cases / pitfalls / patterns>
+"""
+
+def solve():
+    # TODO: implement
+    pass
+
+
+if __name__ == "__main__":
+    # quick test harness
+    print("Running: Sum of Array Elements")
+    # print(solve())
