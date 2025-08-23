0d5450382fb0f8f579b63edb1827f2a268b26b72
Ankit Kumar <mrankitak135@gmail.com>
2025-08-23 18:49:56 +0000

    DSA Practice - Added solutions on 2025-08-23 18:49

--- arrays/sum_of_elements.java
+++ arrays/sum_of_elements.java
@@ -1,21 +1,15 @@
-"""
+// https://www.geeksforgeeks.org/batch/i2it-coding-practice/track/arrays-problems-i2it/problem/sum-all-array-elements
+
+class Solution {
+    public int sum(int arr[], int n) {
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+}"""
 Problem: Sum of Array Elements
 Link: https://www.geeksforgeeks.org/batch/i2it-coding-practice/track/arrays-problems-i2it/problem/sum-all-array-elements
 
-Approach:
-- <Describe your approach here>
-- Time: O(?), Space: O(?)
 
-Notes:
-- <Edge cases / pitfalls / patterns>
-"""
-
-def solve():
-    # TODO: implement
-    pass
-
-
-if __name__ == "__main__":
-    # quick test harness
-    print("Running: Sum of Array Elements")
-    # print(solve())
