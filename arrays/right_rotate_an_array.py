de9200b18b2bfd5a6281cf887db6791a1b45101e
ankit1350 <mrankitak135@gmail.com>
2026-01-07 19:04:48 +0530

    arrays problems

--- arrays/right_rotate_an_array.py
+++ arrays/right_rotate_an_array.py
@@ -1,20 +1,18 @@
-class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
-        n=len(nums)
-        d=k%n
-        self.reverse(0,n,nums)
-        self.reverse(0,d,nums)
-        self.reverse(d,n,nums)
+def rotate(nums,k,n):
+    n=len(nums)
+    d=k%n
+    reverse(0,n,nums)
+    reverse(0,d,nums)
+    reverse(d,n,nums)
+    return nums
         
-    def reverse(self,i,n,a):
-        if i>=n:
-            return
-        a[i],a[n-1]=a[n-1],a[i]
-        self.reverse(i+1,n-1,a)
-        return a
+def reverse(i,n,a):
+    if i>=n-1:
+        return
+    a[i],a[n-1]=a[n-1],a[i]
+    reverse(i+1,n-1,a)
+    return a
 
-        
-        
 arr=list(map(int,input("enter array elements:").split()))
 k=int(input("enter the number of elements to rotate:"))
 n=len(arr)
