436258598d24dbd2b6eef701a36cc9783dc720c1
ankit1350 <mrankitak135@gmail.com>
2026-01-11 20:51:31 +0530

    gpt code

--- arrays/longest-subarray-of-sum-k.py
+++ arrays/longest-subarray-of-sum-k.py
@@ -1,6 +1,23 @@
-arr=[1,2,3,1,1,1,4,5,3]
-n=len(arr)
-i=j=0
-while(i<=n):
-    a.appe
-    j+=1
+class Solution:
+    def longestSubarray(self, nums, k):
+        mp = {}              # prefix_sum : first_index
+        curr_sum = 0
+        max_len = 0
+
+        for i, num in enumerate(nums):
+            curr_sum += num
+
+            # Case 1: subarray from 0 to i
+            if curr_sum == k:
+                max_len = i + 1
+
+            # Case 2: subarray exists in between
+            if curr_sum - k in mp:
+                length = i - mp[curr_sum - k]
+                max_len = max(max_len, length)
+
+            # store first occurrence only
+            if curr_sum not in mp:
+                mp[curr_sum] = i
+
+        return max_len
